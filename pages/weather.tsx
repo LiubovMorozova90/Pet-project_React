6f6b6d94140050df36b52cae0a72f9c014fded1b
LiubovMorozova90 <liubov.morozova.90@mail.ru>
2023-07-13 22:36:06 +0300

    feat: WeatherForecast

--- pages/weather.tsx
+++ pages/weather.tsx
@@ -1,5 +1,10 @@
 import WeatherForecast from 'components/WeatherForecast';
 
 export default function Weather() {
-  return <WeatherForecast />;
+  return (
+    <>
+      <WeatherForecast cityName='moscow'/>
+      <WeatherForecast cityName='washington'/>
+      <WeatherForecast cityName='new york'/>
+    </>);
 }
--- src/components/WeatherForecast/index.tsx
+++ src/components/WeatherForecast/index.tsx
@@ -1,38 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CityName, WeatherForecastType } from './types';
+import style from './WeatherForecast.module.css';
 
-const fetchWeatherForecast = async (
-  cityName: CityName
-): Promise<WeatherForecastType | undefined> => {
-  const options = { method: 'GET', headers: { accept: 'application/json' } };
-
-  try {
-    const response = await fetch(
-      `https://api.tomorrow.io/v4/weather/forecast?timesteps=1d&location=${cityName}&apikey=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`,
-      options
-    );
-
-    const data = (await response.json()) as WeatherForecastType;
-
-    console.log(data);
-
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+type WeatherForecastProps = {
+  cityName: CityName;
 };
 
-// Пример вызова - fetchWeatherForecast('moscow')
-// Не забудь что это асинхронная функция, она возвращает промиз
+const WeatherForecast = ({ cityName }: WeatherForecastProps) => {
+  const [weatherData, setWeatherData] = useState<WeatherForecastType | undefined>(undefined);
 
-const WeatherForecast = () => {
-  // тут будет логика получения погоды.
-  // сначала нужно будет получить погоду в хуке useEffect
-  // после положить полученные данные в стейт с помощью хука useState
+  useEffect(() => {
+    const fetchWeatherForecast = async () => {
+      const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+      try {
+        const response = await fetch(
+          `https://api.tomorrow.io/v4/weather/forecast?timesteps=1d&location=${cityName}&apikey=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`,
+          options
+        );
+
+        const data = (await response.json()) as WeatherForecastType;
+
+        console.log('API Response:', data);
+
+        setWeatherData(data);
+      } catch (error) {
+        console.log('API Error:', error);
+      }
+    };
+
+    fetchWeatherForecast();
+  }, [cityName]);
 
   return (
     <div>
-      <h1>Weather Forecast</h1>
+      {weatherData ? (
+        <div className={style['weather-list']}>
+          <h2 className={style['weather-item']}>Город: {weatherData?.location?.name}</h2>
+          <p className={style['weather-item']}>Температура: {weatherData?.timelines?.daily[0]?.values?.temperatureAvg}</p>
+          <p className={style['weather-item']}>Средняя скорость ветра: {weatherData?.timelines?.daily[0]?.values?.windSpeedAvg}</p>
+          <p className={style['weather-item']}>Время восхода: {weatherData?.timelines?.daily[0]?.values?.sunriseTime.toLocaleTimeString()}</p>
+          <p className={style['weather-item']}>Время заката: {weatherData?.timelines?.daily[0]?.values?.sunsetTime.toLocaleTimeString()}</p>
+        </div>
+      ) : (
+        <p>Loading weather data...</p>
+      )}
     </div>
   );
 };
