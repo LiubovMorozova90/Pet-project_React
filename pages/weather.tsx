fa3f1dcfe0361a5da6605d0687da12f127870872
LiubovMorozova90 <liubov.morozova.90@mail.ru>
2023-07-17 13:50:14 +0300

    add select to select a city

--- pages/weather.tsx
+++ pages/weather.tsx
@@ -1,10 +1,27 @@
+import React, { useState } from 'react';
 import WeatherForecast from 'components/WeatherForecast';
 
+import styles from 'styles/Home.module.css';
+
 export default function Weather() {
+  const [selectedCity, setSelectedCity] = useState('moscow');
+
+  const handleCityChange = (e) => {
+    setSelectedCity(e.target.value);
+  };
+
   return (
-    <>
-      <WeatherForecast cityName='moscow'/>
-      <WeatherForecast cityName='washington'/>
-      <WeatherForecast cityName='new york'/>
-    </>);
+    <div className={styles['weather-section']}>
+      <div className={styles['weather-section_options']}>
+        <p>Пожалуйста, выберите город</p>
+        <select value={selectedCity} onChange={handleCityChange}>
+          <option value="moscow">Москва</option>
+          <option value="washington">Вашингтон</option>
+          <option value="new york">Нью-Йорк</option>
+        </select>
+      </div>
+
+      <WeatherForecast cityName={selectedCity} />
+    </div>
+  );
 }
