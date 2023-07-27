d86ec3e17cc727668af4a6b91e186d73d513f828
LiubovMorozova90 <liubov.morozova.90@mail.ru>
2023-07-27 14:52:15 +0300

    feat props

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -1,8 +1,22 @@
 import React, { PropsWithChildren } from 'react';
 import { useFormik } from 'formik';
 
-type Props = {
+interface Props extends PropsWithChildren {
   label?: string;
   errorMessage?: string;
   isErrorVisible?: boolean;
+}
+
+const Field = (props: Props) => {
+  return (
+    <div>
+      <label>{props.label}</label>
+      {props.children}
+      {props.isErrorVisible && (
+        <div style={{ color: 'red' }}>{props.errorMessage}</div>
+      )}
+    </div>
+  );
 };
+
+export default Field;
